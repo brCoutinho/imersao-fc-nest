9f81f5f3f3ff43f5818b32b81d48b6781edc61c7
Thiago Coutinho <thiago.coutinho@summit-bra.com>
2022-02-08 10:15:04 -0300

    connection mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [TweetsModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root@db/analytics?authSource=admin',
+      {
+        useNewUrlParser: true,
+      },
+    ),
+    TweetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

32a84cdab9d62d0de94b87949845f007cf5babe9
Thiago Coutinho <thiago.coutinho@summit-bra.com>
2022-02-08 10:44:05 -0300

    tweet schema

--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -1,8 +1,13 @@
+import { Tweet, TweetSchema } from './schemas/tweet.schema';
 import { Module } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Tweet.name, schema: TweetSchema }]),
+  ],
   controllers: [TweetsController],
   providers: [TweetsService],
 })
